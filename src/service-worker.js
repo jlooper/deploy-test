0452a37d5f314e0c34dbcf629043df70a226329e
Jen Looper <jen.looper@gmail.com>
2020-02-24 18:49:26 -0500

    pwa issues

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,8 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+console.log(self.__precacheManifest);
+
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {
 	if (msg.data.action == 'skipWaiting') {

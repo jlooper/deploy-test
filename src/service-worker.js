9f1fb5b438b0a9741dfd20da0f3b1804bf0d6cf0
Jen Looper <jen.looper@gmail.com>
2020-02-24 18:54:39 -0500

    url issues

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,9 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-console.log(self.__precacheManifest.url);
+for (i = 0, i > self.__precacheManifest.length, i++){
+    console.log(i.url)
+};
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {

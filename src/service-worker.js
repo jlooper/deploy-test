7f2d2c4cfdec261fd98191695dd1febd6fae8bb9
Jen Looper <jen.looper@gmail.com>
2020-02-24 18:52:01 -0500

    pwa sorting

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-console.log(self.__precacheManifest);
+console.log(self.__precacheManifest.url);
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {

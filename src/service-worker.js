9c7181da048e10222e77b825d2d503a8340fa633
Jen Looper <jen.looper@gmail.com>
2020-02-24 18:20:13 -0500

    precaching edit

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,4 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.

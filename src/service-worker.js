4d5da53aaef981edb7ae4de02077e270ffffde8a
Jen Looper <jen.looper@gmail.com>
2020-02-24 19:08:19 -0500

    fix for manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,11 @@
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
 for (var i = 0; i < self.__precacheManifest.length; i++) {
 	console.log(self.__precacheManifest[i].url);
+	if (self.__precacheManifest[i].url != './web.config') {
+		self.__precacheManifest = [].concat(self.__precacheManifest || []);
+	}
 }
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {

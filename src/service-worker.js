ca09e8bced6a1022598ae31b174575e831e635cd
Jen Looper <jen.looper@gmail.com>
2020-02-24 19:00:43 -0500

    fixing array loop

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,9 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-for (i = 0, i > self.__precacheManifest.length, i++){
-    console.log(i.url)
-};
+for (var i = 0; i < self.__precacheManifest.length; i++) {
+	console.log(i.url);
+}
 
 // install new service worker when ok, then reload page.
 self.addEventListener('message', msg => {

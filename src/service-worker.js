ef66047c85a440a719220a76278c7dd1b81df7b8
Jen Looper <jen.looper@gmail.com>
2020-02-24 19:04:21 -0500

    loop fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 for (var i = 0; i < self.__precacheManifest.length; i++) {
-	console.log(i.url);
+	console.log(self.__precacheManifest[i].url);
 }
 
 // install new service worker when ok, then reload page.
